12b412854bbe10500e3f281f03e104c328c89cba
AbhijeetDamodar1001 <2026aim1001@iitrpr.ac.in>
2026-08-09 22:47:48 +0530

    Add header file for triangle functions

---
+++ assignment_02/src/triangle.h
@@ -0,0 +1,14 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include <vector>
+
+using namespace std;
+
+int countTriangles(
+    const vector<int>& offset,
+    const vector<int>& edges,
+    vector<vector<int>>& triangles
+);
+
+#endif
